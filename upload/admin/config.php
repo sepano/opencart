eddb0e303bb291c8b0a20aef8bd2cce5dcaf326a
root <root@sepano01>
2015-12-30 16:36:23 +0100

    clean install

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.appchar.com/admin/');
+define('HTTP_CATALOG', 'http://opencart.appchar.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.appchar.com/admin/');
+define('HTTPS_CATALOG', 'http://opencart.appchar.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart/upload/admin/');
+define('DIR_SYSTEM', '/var/www/opencart/upload/system/');
+define('DIR_LANGUAGE', '/var/www/opencart/upload/admin/language/');
+define('DIR_TEMPLATE', '/var/www/opencart/upload/admin/view/template/');
+define('DIR_CONFIG', '/var/www/opencart/upload/system/config/');
+define('DIR_IMAGE', '/var/www/opencart/upload/image/');
+define('DIR_CACHE', '/var/www/opencart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart/upload/system/storage/download/');
+define('DIR_LOGS', '/var/www/opencart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/opencart/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/opencart/upload/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/opencart/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencartdb');
+define('DB_PASSWORD', 'XBBdq2QrcQFqj7QU');
+define('DB_DATABASE', 'opencartdb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
